83b701dfd7f97d901014a2b46850164ccc45026f
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-05 06:35:42 +0000

    declaring variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 108907
+let accountEmail = "rizveahmed456@gmail.com"
+var accountPassword = "PhoneixDairy"
+accountCity = "Dhaka"
+let accountState;
+
+
+
+
+accountEmail = "okay"
+accountPassword = "some12443"
+accountCity = "Monpura"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
+console.log(accountPassword)
+
+// Prefer not to use Var because variables declared using the var keyword are either globally or functionally scoped, they do not support block-level scope. This means that if a variable is defined in a loop or in an if statement it can be accessed outside the block and accidentally redefined leading to a buggy program.
+
+
