c8f180d36ad37f87047c96c44bb174fe4e10e704
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-05 09:34:08 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Consider all code in a new version
+
+//alert(3 + 3) // We are using nodejs, Not browser
+
+console.log(3 + 
+    
+    
+    3) // Code readability should be high
+
+    console.log("okay")
+
+    
+    let age = 18
+    let name = "Rizve"
+    let isLoggedIn = true;
+    let ok = null;
+
+    console.log(typeof ok );
+
+
+//number = 2 ^ 53
+//bigint = for large numbers
+//string = "assembly of characters"
+//boolean = true or false
+//null = stand alone value (type = object)
+//undefined = kindda place holder for a value which is not defined yet (type = undefined)
+//symbol = uniquness 
+// "use strict";   treat all code as newer version of JS
+
