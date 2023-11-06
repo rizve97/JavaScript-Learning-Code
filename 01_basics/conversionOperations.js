ee6e4a5f17a7c6a017348337d8de5bbd7ee08a24
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-06 06:12:28 +0000

    conversion operation

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -26,5 +26,35 @@ let someNumber = 1234
 
 let stringCon = String(someNumber);
 
-console.log(stringCon);
-console.log(typeof stringCon);
+//console.log(stringCon);
+//console.log(typeof stringCon);
+
+
+// ********************** Operations *************************
+
+let ok = 566
+let negValue = -(ok);
+//console.log(negValue);
+
+let str1 = "Hello"
+let Str2 = " Boss"
+let str3 = str1 +Str2; 
+//console.log(str3);
+
+//console.log("2" + "2" - "2");
+//console.log(2 + 5+ "3");
+//console.log("3" + "5" + "2");
+// If first attribute is string then it will treat all of them as string
+
+let x = 3;
+const y = x++;
+
+//console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+//console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
