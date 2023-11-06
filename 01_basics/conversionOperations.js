23e623698d0efbb5a9b41c03114ef658a35e1955
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-06 04:09:48 +0000

    conversion operation

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,30 @@
+let score = null
+
+let valueInNumber = Number(score);
+
+//console.log(typeof (score));
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanisLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 1234
+
+let stringCon = String(someNumber);
+
+console.log(stringCon);
+console.log(typeof stringCon);
