471843dd916de548dd4dc5d6fce4793dca6971b9
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-07 05:24:50 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,17 @@
+//console.log(2>1);
+
+//console.log("s" > 2);
+//console.log("2" > 1); #Here we can see that it takes "2" as a number and make copmrasion
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+
+//console.log(undefined > 0);
+//console.log(undefined == 0);
+//console.log(undefined >= 0);
+
+// ****************WE SHOULD STOP DOING THIS TYPE OF CONVERSION *******************************
+
+
+console.log("2" === 2); // Here we can see === check the dataTypes also 
