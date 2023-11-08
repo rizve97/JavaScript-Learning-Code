457ceec3459e5839d117bc97e8832b6536a30b4b
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-08 07:28:22 +0000

    array1

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -1,1 +1,36 @@
-console.log("hbfc")
+const anArray = [0, 1, 2, 3, 4, 5];
+//console.log(anArray);
+
+//const ann_array = new Array("dbhj", "gvfdhg", "gbjbv", "vbv", true, 01);
+//console.log(ann_array[2]);
+
+//Array Methods
+
+//ann_array.push(4);
+//console.log(ann_array);
+
+// anArray.pop(); //# This will cut the last elements of array
+// anArray.push(8, 10);  // This will add elements to the array
+
+
+//anArray.unshift(9);
+//anArray.shift();
+// console.log(anArray.includes(9));
+// console.log(anArray.indexOf(10));
+
+//################################ Slice and Splice ##########################################
+
+
+console.log("A", anArray);
+
+const nArray = anArray.slice(2, 4);
+
+console.log(nArray);
+console.log("B", anArray);
+
+
+const tArray = anArray.splice(2, 3);
+
+console.log(tArray);   //It will manupulate the whole array
+console.log("C", anArray);
+
