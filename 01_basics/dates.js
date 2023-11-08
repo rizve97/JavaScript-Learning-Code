7860efea8a43dbeb70970d81c1e78e4cc6555b64
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-08 05:42:45 +0000

    dateInJS

---
+++ 01_basics/dates.js
@@ -0,0 +1,17 @@
+// const myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toUTCString());
+// console.log(typeof myDate);
+
+let aDate = new Date(2023, 0, 23);
+// console.log(aDate.toDateString());
+
+console.log(aDate.getFullYear());
+//console.log(aDate.getSeconds());
+console.log(aDate.getMonth());
