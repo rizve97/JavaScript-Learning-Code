5e371c1d0f8e5cba5ebecd6572c379ac4248c767
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-08 04:46:52 +0000

    numbers and math

---
+++ 01_basics/numbers_and_math.js
@@ -0,0 +1,28 @@
+const score = 100;
+
+//console.log(typeof score);
+
+const okay = new Number(200);
+
+
+//console.log(okay.toString().length);
+//console.log(typeof okay);
+
+
+const otherNumber = 7484.999;
+//console.log(otherNumber.toPrecision(4));
+
+const money = 1000000;
+//console.log(money.toLocaleString('en-BD'));
+
+
+//######################## MATH #################################
+
+
+//console.log(Math);
+//console.log(Math.abs(-10));
+//console.log(Math.round(10.50)); // It will take numbers like 10.5 = 11 and 10.4 = 10
+//console.log(Math.ceil(10.01));  // It wil take increase number like 10.01 = 11
+//console.log(Math.floor(10.99)); // It will take the minimum value like 10.99 = 10
+
+console.log(Math.random());
