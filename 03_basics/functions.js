4a5e012873cdeba377cef33825a0b3eec28ee365
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-09 06:07:32 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,33 @@
+
+
+ function some(){
+
+    console.log("R");
+    console.log("I");
+    console.log("Z");
+    console.log("V");
+    console.log("E");
+}
+// some()
+
+// function addTwonumbers(num1, num2){
+
+//     console.log(num1 + num2);
+// }
+
+// addTwonumbers(2,3)
+
+// function addTwonumbers(num1, num2){
+
+//     let result = num1 + num2;
+//     return result;
+// }
+// const result = addTwonumbers(2,3);
+// // console.log("Result:",result);
+
+function userLogin(userName){
+
+    return `${userName}, just logged in`;
+}
+
+console.log(userLogin("Rizve"));
