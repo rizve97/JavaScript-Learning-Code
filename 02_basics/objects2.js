43d2c0a18ffa8a9babd98863d9c5327e9412e78d
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-09 04:45:01 +0000

    objects

--- 02_basics/objects2.js
+++ 02_basics/objects2.js
@@ -32,9 +32,29 @@ const obj3 = {...obj1, ...obj2, ...obj4}  // 90% time we will use this syntax.
 // console.log(tinderUser)
 // console.log(Object.keys(tinderUser))
 // console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty("kala")); // To check the property
+// console.log(tinderUser.hasOwnProperty("kala")); // To check the property
+
+
+
+const course = {
+    courseName : "structured programming",
+    coursePrice : 1999,
+    couseHour : 14,
+
+}
+
+const {courseName: CN} = course 
+
+console.log(CN);
+
+// {
+//    "name": "riju"
+//     "Id" : "12345"
+//     "email" : "riju@gmjjail.com"  // its a formal of api 
+
+// }
 
 
 
