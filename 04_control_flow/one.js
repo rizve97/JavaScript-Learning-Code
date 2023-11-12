a1bc26025531754e055f88b1e5e7528036935bda
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-12 06:43:43 +0000

    control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,39 @@
+// const temp = 50
+
+// if (temp <= 49){
+
+//     console.log("executed")
+// }
+
+// console.log("greater than 50")
+
+// let score = 200
+// if (score> 100){
+//     const power = "fly"
+//     console.log(`user power:, ${power}`)
+// }
+
+
+// let balance = 1000
+
+// // if (balance> 100) console.log("hoise")
+
+// if (balance < 200){
+//     console.log("less than")
+// } else if(balance < 500) {
+//     console.log("less than 500")
+// } else if (balance < 900){
+//     console.log("less than 900")
+// }else{
+//     console.log("balance is greater than 900")
+// }
+
+
+const isLoggedIn = true;
+const debitCard = true;
+
+if(isLoggedIn  && debitCard){
+    console.log("you can in our website")
+} else {
+    console.log(" sorry you cant shopping with us")
+}
