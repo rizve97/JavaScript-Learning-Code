1bab13b978f7c8007a49b1e6624971cbd98434f2
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-12 06:57:50 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+let month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+
+        case 2:
+            console.log("february");
+            break;
+
+            case 3:
+                console.log("march");
+                break;
+        case 4:
+        console.log("april");
+        break;        
+
+    default:
+        console.log("default");
+        break;
+}
