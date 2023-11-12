e0fb62a7f2fe3cdc7f8040f32912a8ac6b347983
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-12 04:22:23 +0000

    scopes

---
+++ 03_basics/scopes.js
@@ -0,0 +1,53 @@
+
+// // let a = 200
+// // if(true){
+// //     let a = 10
+// //     const b = 20
+// //     console.log("Inner:", a);
+ 
+// // }
+// //console.log(a);
+// //console.log(b);
+// //console.log("Outer:", a);
+
+        
+        
+// function one(){
+//     const username = "hitesh"
+
+//     function two(){
+//         const web = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+
+//      two()
+
+// }
+// one()
+
+// if(true){
+//     const name = "rizve"
+
+//     if(name === "rizve"){
+//         const kisu = " well"
+//         console.log(name + kisu)
+
+//     }
+//     //console.log(kisu)
+//     console.log(name)
+// }
+//  console.log(name)
+
+//++++++++++++++++++++ Interesting+++++++++++++++++++++++++++++
+
+console.log(addval(5))
+function addval(num){
+    return num + 1
+}
+
+
+const valtwo = function(number){
+    return number + 2
+}
+console.log(valtwo(5))
