1ce5b182b4370381dd68843a9afac174421fe38c
Rizve Ahmed <143622697+rizve97@users.noreply.github.com>
2023-11-14 04:41:15 +0000

    iterations

---
+++ 05_iterations/six.js
@@ -0,0 +1,52 @@
+// const coding = ["Js", "Python", "C++", "Php", "Ruby"]
+
+// const values = coding.forEach( function (item){
+
+//     console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+
+const mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let newnums = mynums.filter((num)=> num > 5 )
+
+// console.log(newnums);
+
+const newnums = []
+
+mynums.forEach( (num) => {
+    if (num > 4) {
+        newnums.push(num)
+    }
+} )
+
+console.log(newnums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  //const userbooks = books.filter((bk) => bk.genre == "History" )
+
+  const userbooks = books.filter((bk) => {
+    if(bk.publish > 2000)
+    return bk
+
+  })
+  console.log(userbooks);
+
+
+
+
